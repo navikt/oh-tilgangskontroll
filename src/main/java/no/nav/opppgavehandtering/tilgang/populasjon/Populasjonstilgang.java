c726bff7bfd8a0b9db23e62d65b46eeb9f4f3cee
torerei <tor.erik.eide@nav.no>
2025-06-28 08:35:49 +0200

    Fikser default logging + skrur på meldingslogging i dev midlertidig for feilsøk

--- src/main/java/no/nav/opppgavehandtering/tilgang/populasjon/Populasjonstilgang.java
+++ src/main/java/no/nav/opppgavehandtering/tilgang/populasjon/Populasjonstilgang.java
@@ -1,7 +1,9 @@
 package no.nav.opppgavehandtering.tilgang.populasjon;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.WebApplicationException;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.reactive.ClientWebApplicationException;
 import org.jboss.resteasy.reactive.RestResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,17 +29,23 @@ public class Populasjonstilgang {
         try {
             log.info("Kontrollerer populasjonstilgang for {} identer", foresporsler.size());
             PopulasjonstilgangResponse populasjonstilgangResponse = tilgangsmaskinenRestClient.kontrollerTilgang(navident, foresporsler);
-            return populasjonstilgangResponse.resultater().stream().map(r -> switch(RestResponse.Status.fromStatusCode(r.status())) {
+            return populasjonstilgangResponse.resultater().stream().map(r -> switch (RestResponse.Status.fromStatusCode(r.status())) {
                 case RestResponse.Status.NO_CONTENT -> new PopulasjonstilgangResultat(true, r.personident(), null);
-                case RestResponse.Status.NOT_FOUND -> new PopulasjonstilgangResultat(false,  r.personident(), new PopulasjonstilgangResultat.TilgangAvvist(AVVIST_PERSON_UKJENT, "Kunne ikke identifisere person"));
-                case RestResponse.Status.FORBIDDEN -> new PopulasjonstilgangResultat(false, r.personident(),  new PopulasjonstilgangResultat.TilgangAvvist(PopulasjonstilgangResultat.Kode.valueOf(r.detaljer().kode()), r.detaljer().begrunnelse()));
+                case RestResponse.Status.NOT_FOUND ->
+                        new PopulasjonstilgangResultat(false, r.personident(), new PopulasjonstilgangResultat.TilgangAvvist(AVVIST_PERSON_UKJENT, "Kunne ikke identifisere person"));
+                case RestResponse.Status.FORBIDDEN ->
+                        new PopulasjonstilgangResultat(false, r.personident(), new PopulasjonstilgangResultat.TilgangAvvist(PopulasjonstilgangResultat.Kode.valueOf(r.detaljer().kode()), r.detaljer().begrunnelse()));
                 default -> {
                     log.warn("Fikk ukjent statuskode fra tilgangsmaskinen: {}", r.status());
                     yield new PopulasjonstilgangResultat(false, r.personident(), new PopulasjonstilgangResultat.TilgangAvvist(PopulasjonstilgangResultat.Kode.AVVIST_UKJENT_AARSAK, "Ukjent svar fra tilgangsmaskinen"));
                 }
             }).collect(Collectors.toMap(PopulasjonstilgangResultat::personident, Function.identity()));
-        } catch(Exception e) {
-            log.warn("Feilet", e);
+        } catch (ClientWebApplicationException e) {
+            String json = e.getResponse().readEntity(String.class);
+            log.warn("Feilkode: {}, Kunne ikke kontrollere populasjonstilgang: {}", e.getResponse().getStatus(), json, e);
+            throw new IllegalStateException(e);
+        } catch (Exception e) {
+            log.warn("Teknisk feil", e);
             throw new IllegalStateException(e);
         }
     }
