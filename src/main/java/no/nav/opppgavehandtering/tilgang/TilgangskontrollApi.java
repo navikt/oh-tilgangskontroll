ba4f9feae7d9ae9b92422e078772a7ec03432b55
torerei <tor.erik.eide@nav.no>
2025-07-02 10:18:51 +0200

    Håndtere input uten personident

--- src/main/java/no/nav/opppgavehandtering/tilgang/TilgangskontrollApi.java
+++ src/main/java/no/nav/opppgavehandtering/tilgang/TilgangskontrollApi.java
@@ -9,6 +9,7 @@ import no.nav.opppgavehandtering.tilgang.populasjon.PopulasjonstilgangResultat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -27,7 +28,12 @@ public class TilgangskontrollApi {
     public Set<TilgangskontrollResultat> kontrollerTilgang(TilgangskontrollRequest request) {
         log.info("Kontrollerer tilgang for: {}", request.navident());
         Set<PopulasjonstilgangRequest> populasjonstilgangForesporsler = request.tilgangskontrollDataSet().stream().filter(s -> s.personident() != null).map(s -> new PopulasjonstilgangRequest(s.personident())).collect(Collectors.toSet());
-        Map<String, PopulasjonstilgangResultat> populasjonstilgangResultat = populasjonstilgang.kontroller(request.navident(), populasjonstilgangForesporsler);
+        Map<String, PopulasjonstilgangResultat> populasjonstilgangResultat;
+        if(populasjonstilgangForesporsler.isEmpty()) {
+            populasjonstilgangResultat = new HashMap<>();
+        } else {
+            populasjonstilgangResultat = populasjonstilgang.kontroller(request.navident(), populasjonstilgangForesporsler);
+        }
         return request.tilgangskontrollDataSet().stream().map(input -> {
             PopulasjonstilgangResultat resultatForPersonident = populasjonstilgangResultat.get(input.personident());
             if(resultatForPersonident == null) {
@@ -38,26 +44,5 @@ public class TilgangskontrollApi {
                     resultatForPersonident.harTilgang() ? null : TilgangskontrollResultat.Kode.valueOf(resultatForPersonident.tilgangAvvist().kode().name()),
                     resultatForPersonident.harTilgang() ? null : resultatForPersonident.tilgangAvvist().begrunnelse());
         }).collect(Collectors.toSet());
-        //
-//        log.info("Mappet");
-//        Tuple2<Set<PopulasjonstilgangResultat>, Set<PopulasjonstilgangResultat>> tuple = Uni.combine().all().unis(
-//                        populasjonstilgang.kontrollerReactive(request.navident(),
-//                                populasjonstilgangForesporsler),
-//                        populasjonstilgang.kontrollerReactive(request.navident(),
-//                                populasjonstilgangForesporsler))
-//                .asTuple()
-//                .await().indefinitely();
-
-//        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
-//            StructuredTaskScope.Subtask<Set<PopulasjonstilgangResultat>> resultat = scope.fork(() -> populasjonstilgang.kontroller(request.navident(),
-//                    identer));
-//            StructuredTaskScope.Subtask<Set<PopulasjonstilgangResultat>> resultat2 = scope.fork(() -> populasjonstilgang.kontroller(request.navident(),
-//                    identer));
-//            scope.join();
-//
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-//        return new ArrayList<>();
     }
 }
